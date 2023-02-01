99f6d70d64d6fe23b6b800bec9423bfe92929165
rambriz91 <rambriz91@gmail.com>
2023-02-01 00:28:59 -0800

    completed index.html layout, added styles to the style.css, linked Weather API in script.js.

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+var APIKey = '94f1fc415316d4290d1bcf565d7ea27a'; 
+var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey key}'
+
+var city;
+
+fetch(queryURL)
