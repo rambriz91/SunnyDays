95c4bbc5669bcf5a6d502a1f30c80a839fc3f7d8
rambriz91 <rambriz91@gmail.com>
2023-02-01 22:46:46 -0800

    finished styling in CSS, html elements in place.

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,8 @@
 var APIKey = '94f1fc415316d4290d1bcf565d7ea27a'; 
-var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey key}'
+var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=94f1fc415316d4290d1bcf565d7ea27a'
 
 var city;
 
 fetch(queryURL)
+.then((response) => response.json())
+.then((data)=> console.log(data));
