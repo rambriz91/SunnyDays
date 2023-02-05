3335a379c88c8755e3b8e6a0a002a2aaf77476c1
rambriz91 <rambriz91@gmail.com>
2023-02-04 17:32:37 -0800

    updated event listener

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,22 @@
 var APIKey = '94f1fc415316d4290d1bcf565d7ea27a'; 
-var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +' &lon=' + long +'&appid=' + APIKey +'&units=imperial';
-var geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchBox +'&limit=1&appid=' + APIKey +'';
-var searchBox = document.getElementById('search').value;
+//var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +' &lon=' + long +'&appid=' + APIKey +'&units=imperial';
+var geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q='+ searchBox +'&limit=1&appid=' + APIKey +'';
+var searchBox = document.getElementById('search');
 var newCity = document.getElementById('city');
-
+var searchBtn =document.getElementById('search-btn');
 var city;
 
 function getCoord() {
+    searchBox = searchBox.value
+    console.log(searchBox)
     fetch(geoCode)
 .then((response) => response.json())
-.then((data)=> console.log(data) 
-    // var lon = data.lon.value;
-    // var lat = data.lat.value
-);
+.then((data)=> console.log(data));
 };
 
 // fetch(queryURL)
 // .then((response) => response.json())
 // .then((data)=> console.log(data));
 
-searchBox.addEventListener('click', getCoord)
+searchBtn.addEventListener('click', getCoord)
 
