ec5e74b450a67a3c5672936c6884322268e55851
rambriz91 <rambriz91@gmail.com>
2023-02-04 21:00:23 -0800

    got API's to work together.

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,36 @@
-var APIKey = '94f1fc415316d4290d1bcf565d7ea27a'; 
-//var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +' &lon=' + long +'&appid=' + APIKey +'&units=imperial';
-var geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q='+ searchBox +'&limit=1&appid=' + APIKey +'';
+var APIKey = '94f1fc415316d4290d1bcf565d7ea27a';
+var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +' &lon=' + lon +'&appid=' + APIKey +'&units=imperial';
+var geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchBox + '&limit=1&appid=' + APIKey + '';
 var searchBox = document.getElementById('search');
 var newCity = document.getElementById('city');
-var searchBtn =document.getElementById('search-btn');
+var searchBtn = document.getElementById('search-btn');
 var city;
+var lat;
+var lon;
+
 
 function getCoord() {
-    searchBox = searchBox.value
-    console.log(searchBox)
+    geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchBox.value + '&limit=1&appid=' + APIKey + '';
+    console.log(searchBox.value)
     fetch(geoCode)
-.then((response) => response.json())
-.then((data)=> console.log(data));
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            var lon = data[0].lon;
+            console.log(lon)
+            var lat = data[0].lat;
+            console.log(lat)
+            queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&appid=' + APIKey +'&units=imperial';
+            fetch(queryURL)
+                .then(function(response) {
+                    return response.json();
+                })
+                    .then(function (data) {
+                        console.log(data)
+                    })
+        });
 };
 
 // fetch(queryURL)
